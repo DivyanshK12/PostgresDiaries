f4492690d3acbd5b56c548b6a921f52e64b88508
Divyansh Kharbanda <55491427+dkarmy12@users.noreply.github.com>
2021-03-13 15:31:15 +0530

    Delete aggregate.sql

--- aggregate.sql
+++
@@ -1,8 +0,0 @@
--- Display total number of rentals in history
-SELECT COUNT(*) FROM rental;
--- Display  the first and last rental
-SELECT MIN(rental_date) first_rental,
-    MAX(rental_date) last_rental
-FROM rental;
--- Total rental payment
-SELECT SUM(amount) AS total_amount FROM payment;
