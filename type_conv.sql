461791a47d263db250d2a94b5a945a9c5cc8fd2e
Divyansh Kharbanda <55491427+dkarmy12@users.noreply.github.com>
2021-03-13 15:32:44 +0530

    Delete type_conv.sql

--- type_conv.sql
+++
@@ -1,8 +0,0 @@
-SELECT CAST(30 AS BIGINT) ! AS my_answer;
--- Increased readibillity, performance and portability
-
-SELECT ROUND(CAST(4 AS NUMERIC), 5) AS second_answer;
-SELECT ROUND(4.0, 5) AS second_answer;
-
-SELECT SUBSTR(CAST('4321' AS TEXT), 2 ) AS FinalAnswer;
--- In each of these, it automatcially casted by itself
